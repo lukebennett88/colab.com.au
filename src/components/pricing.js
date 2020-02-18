a0eaa6f9374d546f728817068ac7663cd4b547c5
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2020-02-18 15:19:36 +1100

    changed pricing dollar text to gray

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -29,13 +29,16 @@ const Pricing = () => {
               </h3>
               <div className="flex text-xs text-black md:text-base md:block">
                 <p className="ml-2 md:ml-0 first:ml-0 md:mt-2">
-                  <strong>Half Day</strong> $19
+                  <strong>Half Day</strong>{' '}
+                  <span className="text-gray-700">$19</span>
                 </p>
                 <p className="ml-2 md:ml-0 first:ml-0">
-                  <strong>One Day</strong> $29
+                  <strong>One Day</strong>{' '}
+                  <span className="text-gray-700">$29</span>
                 </p>
                 <p className="ml-2 md:ml-0 first:ml-0">
-                  <strong>5 Full Days</strong> $79
+                  <strong>5 Full Days</strong>{' '}
+                  <span className="text-gray-700">$79</span>
                 </p>
               </div>
             </div>
@@ -49,10 +52,12 @@ const Pricing = () => {
               </h3>
               <div className="flex text-xs text-black md:text-base md:block">
                 <p className="ml-2 md:ml-0 first:ml-0 md:mt-2">
-                  <strong>Part Time (2 Days)</strong> $79
+                  <strong>Part Time (2 Days)</strong>{' '}
+                  <span className="text-gray-700">$79</span>
                 </p>
                 <p className="ml-2 md:ml-0 first:ml-0">
-                  <strong>Full Time</strong> $179
+                  <strong>Full Time</strong>{' '}
+                  <span className="text-gray-700">$179</span>
                 </p>
               </div>
             </div>
@@ -66,10 +71,12 @@ const Pricing = () => {
               </h3>
               <div className="flex text-xs text-black md:text-base md:block">
                 <p className="ml-2 md:ml-0 first:ml-0 md:mt-2">
-                  <strong>Weekdays</strong> $29 <sup>ph*</sup>
+                  <strong>Weekdays</strong>{' '}
+                  <span className="text-gray-700">$29</span> <sup>ph*</sup>
                 </p>
                 <p className="ml-2 md:ml-0 first:ml-0">
-                  <strong>Weekends</strong> $49 <sup>ph*</sup>
+                  <strong>Weekends</strong>{' '}
+                  <span className="text-gray-700">$49</span> <sup>ph*</sup>
                 </p>
               </div>
               <p className="-mt-4 text-xs font-light md:mt-2">
