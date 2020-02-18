5a96faaf7c1e05915f73da6a6f5ab86051e01489
Andrew Phelps <59522519+NOP-slide@users.noreply.github.com>
2020-02-18 13:05:13 +1100

    add map component

--- src/components/map.js
+++ src/components/map.js
@@ -6,14 +6,14 @@ const Map = () => {
   const { site } = useGraphql();
 
   return (
-    <div className="w-full px-6 md:w-1/2">
+    <div className="w-full px-4 md:w-1/2 map-clip-path">
       <iframe
         src={site.siteMetadata.googleMapsEmbedUrl}
         title="location"
         frameBorder={0}
         style={{ border: 0 }}
         allowFullScreen
-        className="w-full h-64"
+        className="w-full h-72"
       />
     </div>
   );
