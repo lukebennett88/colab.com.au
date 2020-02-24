19bda97b02a6388dde6505965bf801a67ed9fadd
Luke Bennett <hello@lukebennett.com.au>
2020-02-25 09:59:38 +1100

    Style hero image

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,10 +10,7 @@ const Hero = () => {
   const data = useGraphQL();
 
   return (
-    <article
-      id="home"
-      className="relative flex items-end justify-end min-h-screen"
-    >
+    <article id="home" className="relative flex flex-col min-h-screen">
       <div className="absolute inset-0">
         <Image
           fluid={data.heroImage.childImageSharp.fluid}
@@ -21,14 +18,14 @@ const Hero = () => {
         />
       </div>
       <Logo
-        className="absolute top-0 left-0 px-6 pt-24 w-72 md:hidden"
-        pathClass="fill-current text-fuschia"
+        className="relative flex-shrink-0 w-64 px-6 m-auto transform translate-y-16 md:hidden"
+        pathClass="fill-current text-white"
       />
-      <div className="relative px-6 py-4 md:py-16 md:px-24">
-        <h1 className="text-4xl font-bold leading-none text-right text-white uppercase md:text-5xl font-display">
-          Port Macquarie's
-          <br /> Fresh Community Hub
-          <br /> and Work Space
+      <div className="relative w-full px-6 py-4 mt-auto md:py-16 md:px-24">
+        <h1 className="text-3xl font-bold leading-none text-center text-white uppercase md:text-right sm:text-4xl md:text-5xl font-display">
+          Port&nbsp;Macquarie's
+          <br className="hidden md:inline" /> Fresh Community Hub
+          <br className="hidden md:inline" /> and Work&nbsp;Space
         </h1>
       </div>
       <div className="fixed bottom-0 right-0 z-20 mb-56 mr-5 md:mr-12 md:pr-3">
